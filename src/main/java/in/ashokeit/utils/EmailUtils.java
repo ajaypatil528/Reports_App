db73cf6fb31cc54db86393013f1e7a904d1c1d9d
ajaypatil528 <124812032+ajaypatil528@users.noreply.github.com>
2023-06-23 14:21:07 -0400

    Update EmailUtils.java

--- src/main/java/in/ashokeit/utils/EmailUtils.java
+++ src/main/java/in/ashokeit/utils/EmailUtils.java
@@ -1,5 +1,41 @@
 package in.ashokeit.utils;
 
-public class EmailUtils {
+import java.io.File;
 
+import javax.mail.internet.MimeMessage;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailUtils {
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public boolean sendEmail(String subject, String body, String to, File f) {
+		
+		try {
+			//SimpleMessage class is uesd to send plain emails (text format)
+			//MimeMessage class used when I want to send Emails with attachment
+			
+			MimeMessage mimeMsg = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMsg, true);
+			
+			helper.setSubject(subject);
+			helper.setText(body, true);
+			helper.setTo(to);
+			
+			helper.addAttachment("Plans-Info", f);
+			//helper.addAttachment("plans.xls", f);
+			//helper.addAttachment("plans.pdf", f);
+			mailSender.send(mimeMsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
 }
