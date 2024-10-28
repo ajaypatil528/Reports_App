7a50cc36e147309e638156fa2d4565b6010ed3fb
ajay patil <124812032+ajaypatil528@users.noreply.github.com>
2024-10-28 16:14:39 -0400

    Update ServletInitializer.java

--- src/main/java/in/ashokeit/ServletInitializer.java
+++ src/main/java/in/ashokeit/ServletInitializer.java
@@ -1,4 +1,4 @@
-package in.ashokeit;
+package in.reports;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
